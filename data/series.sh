ad48db1b316b7010c9152943c6b25f16e9396a69
Miguel Aréjula Aisa <arejula10@gmail.com>
2023-04-12 16:17:05 +0200

    correcion datos series

--- data/series.sh
+++ data/series.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat DatosPeliculas_copia.csv | cut -d ';' -f 7,8,20,21 | cut -d '"' -f 1 | sort | uniq  >series.csv
+cat DatosPeliculas_copia3.csv | cut -d ';' -f 7,8,20,21 | cut -d '"' -f 1 | sort | uniq  >series.csv
