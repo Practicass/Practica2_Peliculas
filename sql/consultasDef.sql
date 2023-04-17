993007acd3d017c3e22b74487042d838bf7cc232
pabloangusto <pabloangusto@gmail.com>
2023-04-17 15:36:04 +0200

    fin?

--- sql/consultasDef.sql
+++ sql/consultasDef.sql
@@ -14,30 +14,6 @@ WHERE   O.IdObra = G.IdObra
 
 
 --Listar el nombre de los directores que han dirigido al menos seis series distintas en la década de los 90.
-
-SELECT PERS.Nombre AS nomPer, COUNT(S.IdObra) AS contador
-FROM OBRAS O, SERIES S, DIRECTORES D
-JOIN PERSONAS PERS ON PERS.IdPer = D.IdPer
-WHERE ((O.AgnoEstreno < 1990
-    AND (S.fin IS NULL OR S.fin >= 1990)) OR (O.AgnoEstreno>=1990 and O.Agnoestreno < 2000))
-    AND S.IdObra = O.IdObra
-    AND O.IdObra = D.IdObra
-    AND PERS.Nombre IN (
-        SELECT PERS2.Nombre
-        FROM PERSONAS PERS2
-        JOIN DIRECTORES D2 ON D2.IdPer = PERS2.IdPer
-        JOIN OBRAS O2 ON O2.IdObra = D2.IdObra
-        JOIN SERIES S2 ON O2.IdObra = S2.IdObra
-        WHERE ((O2.AgnoEstreno < 1990
-    AND (S2.fin IS NULL OR S2.fin >= 1990)) OR (O2.AgnoEstreno>=1990 and O2.Agnoestreno < 2000))
-        GROUP BY PERS2.Nombre
-        HAVING COUNT(DISTINCT O2.IdObra) >= 6
-    )
-GROUP BY PERS.Nombre
-HAVING COUNT(DISTINCT O.IdObra) >= 6
-ORDER BY contador DESC;
-
-
 SELECT PERS2.Nombre, count(S2.IdObra) as num_veces
         FROM PERSONAS PERS2, DIRECTORES D2, OBRAS O2, SERIES S2
         WHERE ((O2.AgnoEstreno < 1990
@@ -49,17 +25,19 @@ SELECT PERS2.Nombre, count(S2.IdObra) as num_veces
         HAVING COUNT(DISTINCT O2.IdObra) >= 6
 
 
-
-
-
-SELECT A.Personaje, COUNT(A.Personaje) AS NumeroPersonajes
+--Obtener el número de personajes distintos que han sido interpretados por al menos cuatro actores o actrices 
+--distintos y que aparecen en películas de alguna saga (es decir, que son secuelas/precuelas unas de otras):
+SELECT COUNT(DISTINCT A2.Personaje) as "Numero de Personajes"
+FROM ACTORES A2
+WHERE A2.personaje IN 
+(SELECT A.Personaje
 FROM ACTORES A
 WHERE A.idObra IN (
-    SELECT DISTINCT c1.idObra
-    FROM CONEXION c1, CONEXION c2
-    WHERE c1.idObra = c2.idObra2 
-                  AND  c1.Tipo IN ('follows', 'followed by')
-     AND c2.Tipo IN ('follows', 'followed by') 
+    	SELECT DISTINCT c1.idObra
+    	FROM CONEXION c1, CONEXION c2
+   	 WHERE c1.idObra = c2.idObra2 
+                  	AND  c1.Tipo IN ('follows', 'followed by')
+     	AND c2.Tipo IN ('follows', 'followed by') 
 )  AND A.Personaje IS NOT NULL
 GROUP BY A.Personaje
-HAVING COUNT(DISTINCT A.IdPer) >= 4;
+HAVING COUNT(DISTINCT A.IdPer) >= 4);
